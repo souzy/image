05cdac3fa29d2baffc65f8271847362608df2b94
souzy <32474135+souzy@users.noreply.github.com>
2017-11-27 21:33:29 -0800

    Add files via upload

---
+++ image.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <cstring>
+#include <iostream>
+#include <cerrno>
+#include <sstream>
+#include "image.h"
+#include "frame.h"
+
+extern Frame frame;
+
+void Image::load(const char * filename, int w, int h) {
+	this->w = w;
+	this->h = h;
+	pixels = new byte[w * h * 3];
+
+	std::stringstream ss;
+	ss << "ffmpeg ";
+	ss << "-i " << filename << " ";
+	ss << "-f rawvideo ";
+	ss << "-pix_fmt rgb24 ";
+	ss << "-vf scale=" << w << "x" << h << " ";
+	ss << "-";
+
+#ifdef _WIN32
+	FILE * pipe = _popen(ss.str().c_str(), "rb");
+#else
+	FILE * pipe = popen(ss.str().c_str(), "r");
+#endif
+	if (pipe == 0) {
+		std::cout << "error: " << strerror(errno) << std::endl;
+		exit(1);
+	}
+	fread(pixels, 3, w * h, pipe);
+
+	fflush(pipe);
+#ifdef _WIN32
+	_pclose(pipe);
+#else
+	pclose(pipe);
+#endif
+}
+
+void Image::draw() const {
+	for (int i = 0; i < w; ++i) {
+		for (int j = 0; j < h; ++j) {
+			byte r = pixels[j * w * 3 + i * 3 + 0];
+			byte g = pixels[j * w * 3 + i * 3 + 1];
+			byte b = pixels[j * w * 3 + i * 3 + 2];
+			frame.setPixel(x + i, y + j, r, g, b);
+		}
+	}
+}
---
+++ image.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "global.h"
+typedef unsigned char byte;
+
+class Image {
+public:
+	Image() : pixels(0), w(0), h(0), x(0), y(0) { }
+	~Image() { delete [] pixels; }
+	void load(const char * filename, int w, int h);
+	void draw() const;
+
+private:
+	byte * pixels;
+	int w;  // width of image in pixels
+	int h;  // height of image in pixels
+	int x;  // Location of image inside of the frame.
+	int y;
+};
